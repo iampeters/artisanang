97a4b17f7330854361800a505589cd69a38fb0a3
Peters Chikezie <peters.chikezie@bankly.ng>
2020-08-08 17:52:45 +0100

    added sigin in

--- src/components/How.tsx
+++ src/components/How.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import PrimaryTheme from '../themes/Primary'
 import Hero from './Hero'
 import { Icon } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
 
 export default function How() {
+  const history = useHistory();
+
   return (
     <div style={{
       backgroundColor: PrimaryTheme.white,
@@ -67,6 +70,17 @@ export default function How() {
               }}>
                 Get tough on unscrupulous artisans by blowing the whistle on frauds and unprofessional acts on social media via Artisana -Belong to a network of users.
               </h5>
+            </div>
+
+            <div className="col-md-12 text-white text-center mb-3">
+              <button className="btn btn-light" onClick={() => history.push('sign-in')}>
+                <div className="row m-0 justify-content-between align-items-center">
+                  <span className='mr-3'>Sign In</span>
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-arrow-right-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-8.354 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L9.793 7.5H5a.5.5 0 0 0 0 1h4.793l-2.147 2.146z" />
+                  </svg>
+                </div>
+              </button>
             </div>
 
           </div>
